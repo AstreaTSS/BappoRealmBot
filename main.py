9f4f458ea58e73b4951f4eb7b8ff2edc758206b2
Sonic4999 <sonictbm@gmail.com>
2019-11-06 16:17:40 -0500

    A revival; updated to latest info

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import discord, os, datetime
+import discord, datetime, os
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='!?')
@@ -25,6 +25,21 @@ async def ping(ctx):
 
 @bot.command()
 @commands.check(is_mod_or_up)
+async def check_stats(ctx, season):
+    season_x_role = discord.utils.get(ctx.guild.roles, name=f'S{season} Badge')
+    count = 0
+    mes_of_people = "```\n"
+
+    for member in ctx.guild.members:
+        if season_x_role in member.roles:
+            count += 1
+            mes_of_people += (f"{member.display_name}\n")
+
+    await ctx.send(f'There are {count} people that have the S{season} badge.')
+    await ctx.author.send(mes_of_people + "```")
+
+@bot.command()
+@commands.check(is_mod_or_up)
 async def season_add(ctx, season, message_id):
     ori_mess = await ctx.guild.get_channel(596186025630498846).fetch_message(int(message_id))
     ori_timestamp = ori_mess.created_at.timestamp()
@@ -45,7 +60,6 @@ async def season_add(ctx, season, message_id):
 
     await ctx.send("Done! Added " + str(len(season_x_vets)) + " members!")
 
-
 @bot.event
 async def on_member_join(member):
 
@@ -61,21 +75,23 @@ async def on_member_join(member):
 
             await member.kick(reason="Too young account to join Bappo")
         else:
-            mod_role = discord.utils.get(member.guild.roles, name='Moderator')
+            gatekeeper = discord.utils.get(member.guild.roles, name='Gatekeeper')
 
             verify_channel = discord.utils.get(member.guild.channels, name='verify')
 
             await verify_channel.send("Welcome to the Bappo Realm, " + member.mention + "!\n\nYou might have " +
             "noticed that there's not a lot of channels. Well, that's because you have to be verified. To be verified, " +
-            "check out the questions below and answer them. A " + mod_role.mention + " will review them and verify you.\n\n" +
+            "check out the questions below and answer them. A " + gatekeeper.mention + " will review them and verify you.\n\n" +
             "```\nVerification Questions:\n\n1. In what ways did you manage to access the server (via advertising links, friends, etc)?" +
             "\n\n2. Is this your first time taking part in a minecraft community (server, realm, etc), if not, how many past communities " + 
             "have you participated in?\n\n3. Have you ever had any experience in being helpful in a community, and if so, how?\n\n" +
-            "4. How long have you been participating in communities?\n\n5. What are you looking for in joining this server?\n```")
+            "4. How long have you been participating in communities?\n\n5. What are you looking for in joining this server?\n\n6. In your own " +
+            "words, summarize rules 2 and 6 (If you don't understand what this means, explain rules 2 and 6 like you would if explaining it to " +
+            "another person).\n```")
 
 @bot.event
 async def on_message(mes):
-    if mes.channel.id == 631597319103578122 or mes.channel.id == 631327297278050348:
+    if mes.channel.id == 638566668372541441 or mes.channel.id == 631597319103578122:
         owner_role = discord.utils.get(mes.guild.roles, name='Owner')
 
         if not owner_role in mes.author.roles:
