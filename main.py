e8976ed8f77d7e383abd1098789aff96ec193c9c
Sonic4999 <sonictbm@gmail.com>
2019-11-09 22:31:57 -0500

    Added gatekeeper, help cmd

--- main.py
+++ main.py
@@ -14,6 +14,17 @@ def is_mod_or_up(ctx):
         return True
     return False
 
+def is_gatekeeper_or_up(ctx):
+    if is_mod_or_up(ctx):
+        return True
+
+    gatekeeper = ctx.guild.get_role(641619621178245121)
+    member_roles = ctx.author.roles
+
+    if gatekeeper in member_roles:
+        return True
+    return False
+
 @bot.command()
 async def ping(ctx):
     current_time = datetime.datetime.utcnow().timestamp()
@@ -25,12 +36,16 @@ async def ping(ctx):
     await ctx.send(f"Pong! `{ping_discord}` ms from discord.\n`{ping_personal}` ms personally (not accurate)")
 
 @bot.command()
-async def say(ctx, *, mes):
-    await ctx.message.delete()
-    await ctx.send(mes)
+async def say(ctx, *args):
+    if type(args[0]) is discord.TextChannel:
+        channel = args[0]
+        await channel.send(" ".join(args[1:]))
+        await ctx.message.delete()
+    else:
+        await channel.send(" ".join(args))
+        await ctx.message.delete()
 
 @bot.command()
-@commands.check(is_mod_or_up)
 async def check_stats(ctx, season):
     season_x_role = discord.utils.get(ctx.guild.roles, name=f'S{season}')
     if season_x_role == None:
@@ -78,6 +93,21 @@ async def season_add(ctx, season, message_id):
 
 @bot.command()
 @commands.check(is_mod_or_up)
+async def role_id(ctx, role_name):
+    role = discord.utils.get(ctx.guild.roles, name=role_name)
+    
+    if role == None:
+        await ctx.send("Invalid role name!")
+    else:
+        await ctx.send(str(role.id))
+
+@bot.group()
+@commands.check(is_gatekeeper_or_up)
+async def gk(ctx):
+    if ctx.invoked_subcommand is None:
+        await ctx.send('Invalid gatekeeper command passed...')
+
+@gk.command()
 async def verify(ctx, member: discord.Member):
     to_be_verified = discord.utils.get(ctx.guild.roles, name='To Be Verified')
     member_role = discord.utils.get(ctx.guild.roles, name='Member')
@@ -90,20 +120,38 @@ async def verify(ctx, member: discord.Member):
 
         await ctx.send(f"{member.mention} was verified!")
 
-@bot.command()
-async def role_id(ctx, role_name):
-    role = discord.utils.get(ctx.guild.roles, name=role_name)
-    
-    if role == None:
-        await ctx.send("Invalid role name!")
+@gk.command()
+async def kick(ctx, member: discord.Member, *, reason):
+    to_be_verified = discord.utils.get(ctx.guild.roles, name='To Be Verified')
+    if not to_be_verified in member.roles:
+        await ctx.send("You can't kick this user! They don't have the To Be Verified role!")
     else:
-        await ctx.send(str(role.id))
+        await member.kick(reason=f"By {ctx.author.display_name}: {reason}")
+
+@gk.command()
+async def ban(ctx, member: discord.Member, *, reason):
+    to_be_verified = discord.utils.get(ctx.guild.roles, name='To Be Verified')
+    if not to_be_verified in member.roles:
+        await ctx.send("You can't ban this user! They don't have the To Be Verified role!")
+    else:
+        await member.ban(reason=f"By {ctx.author.display_name}: {reason}")
 
 bot.remove_command("help")
 @bot.command()
 async def help(ctx):
-    await ctx.send("There is no help command, suggesting the rare cases where any of the commands are used, "
-    "and how the owner already knows the commands.")
+
+    embed = discord.Embed(colour=discord.Colour(0x4a90e2), description="A list of every command offered by this bot.")
+
+    embed.set_author(name="Bappo Realm Custom Bot", icon_url="https://cdn.discordapp.com/avatars/618993974048194560/9533dc8ab73566f714731f17ed90d913.png")
+
+    embed.add_field(name="Usable By Everyone", value="`help - Displays this.\nping - Pings the bot.\nsay <optional channel, message> - Makes the bot " +
+    "say whatever you command it to. Can specify channel to send.\ncheck_stats <season> - Checks the stats for the season you give. Doesn't support the latest season.`")
+    embed.add_field(name="Gatekeeper+ Commands", value="`gk verify <user> - Verifies the user mentioned.\ngk kick <user, reason> - Kicks the user " + 
+    "mentioned if they aren't verified. A reason is needed, too.\ngk ban <user, reason> - Bans the user mentioned if they aren't verified. A reason is needed, too.`")
+    embed.add_field(name="Mod+ Commands", value="`season_add <season, a message id> - Gives the S<season> role (which must exist beforehand) to everyone " +
+    "who joined before <a message id> was created.\nrole_id <role name> - Gets the ID of <role name)>.`")
+
+    await ctx.send(embed=embed)
 
 @bot.event
 async def on_member_join(member):
