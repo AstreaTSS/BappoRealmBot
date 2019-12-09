f9060148d46498ff50c756296069bf37bf5ee667
Sonic4999 <sonictbm@gmail.com>
2019-12-08 22:30:30 -0500

    Fixed color

--- countdown_class.py
+++ countdown_class.py
@@ -4,6 +4,6 @@ from datetime import datetime
 class Countdown():
     def __init__(self, name, color, channel_id, time):
         self.name = name
-        self.color = discord.Color(int(color))
+        self.color = discord.Color(int(color, 0))
         self.channel_id = channel_id
         self.time = int(time)
