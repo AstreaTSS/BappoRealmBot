72f7ed93c486646b9d784adfff1a7eb8b51d1bd4
Sonic4999 <sonictbm@gmail.com>
2020-01-18 00:16:16 -0500

    Removed 7 day limit

--- cogs/etc.py
+++ cogs/etc.py
@@ -8,32 +8,20 @@ class ETC(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member):
         if not member.bot:
-            current_time = datetime.datetime.utcnow().timestamp()
+            gatekeeper = discord.utils.get(member.guild.roles, name='Gatekeeper')
 
-            if (current_time - member.created_at.timestamp()) <= 604800:
-                try:
-                    await member.send("Your account is too young to join the Bappo Realm Discord server. Try "
-                    "again once your Discord account is 7 days old.")
-                except discord.Forbidden:
-                    owner = await self.bot.application_info().owner
-                    await owner.send(f"{member.mention} has blocked DMs and is being kicked for having too young of an account.")
+            verify_channel = discord.utils.get(member.guild.channels, name='verify')
+            gamertags = discord.utils.get(member.guild.channels, name='gamertags')
 
-                await member.kick(reason="Too young account to join Bappo")
-            else:
-                gatekeeper = discord.utils.get(member.guild.roles, name='Gatekeeper')
-
-                verify_channel = discord.utils.get(member.guild.channels, name='verify')
-                gamertags = discord.utils.get(member.guild.channels, name='gamertags')
-
-                await verify_channel.send("Welcome to the Bappo Realm, " + member.mention + "!\n\nYou might have " +
-                "noticed that there's not a lot of channels. Well, that's because you have to be verified. To be verified, " +
-                "check out the questions below and answer them here. Also, make sure to put your gamertag in " + gamertags.mention + " so that we " +
-                "know who you are in Minecraft.\nA " + gatekeeper.mention + " will review them and verify you.\n\n" +
-                "```\nVerification Questions:\n\n 1. How did you come to know about this realm? Don't just say \"advertising\" if that's true " +
-                "for you; be more specific, like stating from where.\n\n2. How long have you been playing Minecraft? How long have you been on " +
-                "Discord? Have you ever been a problem in Minecraft or Discord?\n\n3. What platform do you play on? Are you aware that this is a " +
-                " Bedrock edition realm, not a Java edition realm?\n\n4. What are RULES 2 and 6 in your own words? If you don't understand what this " + 
-                "means, explain rules 2 and 6 like you would if explaining it to another person.\n```")
+            await verify_channel.send("Welcome to the Bappo Realm, " + member.mention + "!\n\nYou might have " +
+            "noticed that there's not a lot of channels. Well, that's because you have to be verified. To be verified, " +
+            "check out the questions below and answer them here. Also, make sure to put your gamertag in " + gamertags.mention + " so that we " +
+            "know who you are in Minecraft.\nA " + gatekeeper.mention + " will review them and verify you.\n\n" +
+            "```\nVerification Questions:\n\n 1. How did you come to know about this realm? Don't just say \"advertising\" if that's true " +
+            "for you; be more specific, like stating from where.\n\n2. How long have you been playing Minecraft? How long have you been on " +
+            "Discord? Have you ever been a problem in Minecraft or Discord?\n\n3. What platform do you play on? Are you aware that this is a " +
+            " Bedrock edition realm, not a Java edition realm?\n\n4. What are RULES 2 and 6 in your own words? If you don't understand what this " + 
+            "means, explain rules 2 and 6 like you would if explaining it to another person.\n```")
 
     @commands.Cog.listener()
     async def on_message(self, mes):
